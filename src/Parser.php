6291a274da0e94b3d319ae4562c897d62f4517a9
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2015-09-28 09:37:06 +0200

    Added methods for report validation and feed statusses

--- src/Parser.php
+++ src/Parser.php
@@ -39,4 +39,50 @@ class Parser
             'data'          => $data,
         ];
     }
+
+    /**
+     * Check if the feed specified is known in the parser config.
+     * @param  string  $name Current feed name
+     * @return boolean      Return true of false
+     */
+    protected function isKnownFeed($configBase, $feedName)
+    {
+        if (empty(config("{$configBase}.feeds.{$feedName}"))) {
+            return $this->failed(
+                "Detected feed '{$feedName}' is unknown."
+            );
+        }
+        return true;
+    }
+
+    /**
+     * Check and see if a feed is enabled.
+     * @param  string $name Current feed name
+     * @return boolean      Return true of false
+     */
+    protected function isEnabledFeed($configBase, $feedName)
+    {
+        return (config("{$configBase}.feeds.{$feedName}.enabled") === true) ? true : false;
+    }
+
+    /**
+     * Check if all required fields are in the report.
+     * @param  array  $report Report data
+     * @param  string $name   Current feed name
+     * @return boolean        Returns true or fails the parsing
+     */
+    protected function hasRequiredFields($configBase, $feedName, $report)
+    {
+        $columns = array_filter(config("{$configBase}.feeds.{$feedName}.fields"));
+        if (count($columns) > 0) {
+            foreach ($columns as $column) {
+                if (!isset($report[$column])) {
+                    return $this->failed(
+                        "Required field ${column} is missing in the report or config is incorrect."
+                    );
+                }
+            }
+        }
+        return true;
+    }
 }
